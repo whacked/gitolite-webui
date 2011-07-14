c34da98b9379ecb26259704cdd9af9c85243098e
narkisr <narkisr@gmail.com>
2011-07-15 01:28:24 +0300

    cleaning up db file

--- src/gitolite_webui/db.clj
+++ src/gitolite_webui/db.clj
@@ -5,19 +5,15 @@
 	[clojure.contrib.json :as json]
 	[clojure.contrib.datalog.database :as dlog]))
 
-
 (defn save [db-file db]
 	(spit (file db-file) (json/json-str @db)))
 
-(defn schduled-save [db-file db]
+(defn periodical-save [db-file db interval]
 	(let [timer (Timer.)]
-	  (. timer schedule  (proxy [TimerTask] [] (run [] (save db-file db)))  (* 5 1000))))
+	  (. timer schedule  (proxy [TimerTask] [] (run [] (save db-file db)))  (* interval 1000))))
 
 (defn- into-tuple [hash relation]
 	 (into [] (flatten (cons relation (seq hash)))))
-
-#_(dosync db (fn [db] (dlog/add-tuples db 
-						   (map #(into-tuple % relation)  (get-in  db-map [relation :data])))))
 
 (defn- tuples-of [relation]
 	 (into []  (map #(vector (first relation) %) (-> relation (second) (:data)))))
@@ -30,6 +26,5 @@
 	(if (-> db-file (file) (. exists))
 	  (let [db-map (json/read-json (slurp (file db-file))) ]
 	    (doseq [tuple (db-map-tuples db-map)] 
-		     (dosync 
-			 (alter db 
+		     (dosync (alter db 
 			 	  (fn [db] (dlog/add-tuple db (first tuple) (second tuple)))))))))
