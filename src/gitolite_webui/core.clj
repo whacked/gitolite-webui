c02db0c1ce3d67e4bffc225a81790783342829c5
narkisr <narkisr@gmail.com>
2011-07-17 01:57:07 +0300

    adding forms layout and repo access form

--- src/gitolite_webui/core.clj
+++ src/gitolite_webui/core.clj
@@ -1,7 +1,7 @@
 (ns gitolite-webui.core 
     (:use 
     	 compojure.core
-    	[net.cgrand.enlive-html :only [deftemplate defsnippet content clone-for nth-of-type first-child do-> set-attr sniptest at emit*]] )
+    	[net.cgrand.enlive-html :only [deftemplate defsnippet content clone-for nth-of-type first-child do-> set-attr sniptest at emit* content html-resource]] )
     (:require 
       (ring.middleware [multipart-params :as mp])
       (clojure.contrib [duck-streams :as ds])
@@ -11,23 +11,40 @@
 	[compojure.handler :as handler]))
 
 (def *webdir* (str (ds/pwd) "/src/public"))
+
 (defn render [t] (apply str t))
+
 (persist/initialize "gitolite-db")
 
 (deftemplate index "public/index.html" [])
+
 (deftemplate upload-success "public/upload-success.html" [])
 
-(defn upload-file [file]
-  (ds/copy (file :tempfile) (ds/file-str "file.out"))
-  (render (upload-success)))
+(def upload-form (html-resource "public/upload-form.html")) 
 
+(def upload-form (html-resource "public/access-form.html")) 
+
+(deftemplate forms-layout "public/forms-layout.html" [body title]
+		[:body] (content body) 
+		[:title] (content title))
+
+
+(defn process-ssh-upload [{:keys [name email file]}]
+  (persist/persist-key-request name email (slurp (file :tempfile))))
 
 (defroutes main-routes
            (GET  "/" [] (render (index)))
-	     (route/files "/upload-form.html")
+
+	     (GET "/upload-form.html" [] (render (forms-layout upload-form "upload ssh key")))
+	     (GET "/access-form.html" [] (render (forms-layout upload-form "request repository access")))
+
 	     (mp/wrap-multipart-params 
-            (POST "/ssh-upload" {params :params} (upload-file (:file params)))) 
+              (POST "/ssh-upload" {params :params} 
+                (process-ssh-upload params)
+                (render (upload-success)))) 
+
 	     (route/resources "/")
+
 	     (route/not-found "Page not found"))
 
 (def app (handler/site main-routes))
---
+++ src/gitolite_webui/gitolite.clj
@@ -0,0 +1,7 @@
+(ns gitolite-webui.gitolite
+  (:use [clojure.contrib.io :only [file])
+ )
+
+(defn available-repositories []
+   [play1 play2]
+  )
