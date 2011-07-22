4c20103c09bf9fb11856cc3fc8607a4ca3b4af9f
narkisr <narkisr@gmail.com>
2011-07-23 02:11:39 +0300

    gitolite pasring works

--- src/gitolite_webui/gitolite.clj
+++ src/gitolite_webui/gitolite.clj
@@ -1,16 +1,23 @@
 (ns gitolite-webui.gitolite
-  (:use [clojure.contrib.io :only [file]] 
-  	  [clojure.contrib.string :only [split-lines]]
-  	  ))
+    (:use [clojure.contrib.io :only [file]] 
+	    [clojure.contrib.string :only [split-lines split]]
+	    [clojure.contrib.macros :only [letfn- ]]
+	    ))
 
 (defn repo-name [repo-str]
-     (last (re-matches #"repo\s+(.*)" repo-str)))
+	(last (re-matches #"repo\s+(.*)" repo-str)))
 
 (defn- raw-repos [path]
-	(filter #(.startsWith (first %) "repo")
-		  (->> path slurp split-lines (partition-by empty?))))
+	 (filter #(.startsWith (first %) "repo")
+		   (->> path slurp split-lines (partition-by empty?))))
+
+(defn permissions [lines]
+	(let [pair (fn [line] (->> line (split #"\=") (map #(.trim %)) reverse (into [])))] 
+		  (into {} (reduce (fn [m line] (conj m (pair line))) [] lines))))
 
 (defn parse-conf [path]
-	(reduce (fn [m repo] (assoc m (-> repo first repo-name keyword) (rest  repo))  ) {} (raw-repos path)))
+	(reduce (fn [m repo] 
+			(assoc m (-> repo first repo-name keyword) (permissions (rest repo))))
+		  {} (raw-repos path)))
 
 
--- test/gitolite_webui/test/gitolite.clj
+++ test/gitolite_webui/test/gitolite.clj
@@ -5,5 +5,14 @@
 
 (fact (g/repo-name "repo   play-0") => (just "play-0"))
 
-(fact (g/parse-conf "test/resources/gitolite.conf") => (just {}))
+
+(fact (g/parse-conf "test/resources/gitolite.conf") => 
+  (contains 
+    {:play-lasting {"alice" "RW+" "bob" "RW+"}}
+    {:moving-around {"ronenn" "RW+" "jenkins" "RW+"}}
+    {:play-0 {"ronen" "RW+" "foo" "RW+" "bar" "RW+" "bob" "RW+" "alice" "RW+" "jenkins" "R"}}
+    {:testing {"@all" "RW+"} }
+    {:gitolite-admin {"ronenn" "RW+"}} 
+    )	
+  )
 
