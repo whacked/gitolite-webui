24e509b397b82f459ce9680024c142fb27c9fead
narkisr <narkisr@gmail.com>
2011-07-23 23:50:13 +0300

    fixing fact wrong fn called

--- test/gitolite_webui/test/persistency.clj
+++ test/gitolite_webui/test/persistency.clj
@@ -7,7 +7,7 @@
 (fact (p/ssh-pending) => (just (list {:name "ronen" :email "narkisr@gmail.com" :key "ssh-rsa 1234" }))
 	(against-background (before :checks (p/persist-key-request "ronen" "narkisr@gmail.com" "ssh-rsa 1234"))))
 
-(fact (p/ssh-pending) => (just (list {:name "ronen" :repo "play-0" }))
+(fact (p/access-pending) => (just (list {:name "ronen" :repo "play-0" }))
 	(against-background (before :checks (p/persist-repo-request "ronen" "play-0"))))
 
 
