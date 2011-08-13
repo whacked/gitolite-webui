2355105bf44aa29d64ce10d688188aaac8414533
narkisr <narkisr@gmail.com>
2011-08-13 18:00:51 +0300

    Testing request removal

--- test/gitolite_webui/test/persistency.clj
+++ test/gitolite_webui/test/persistency.clj
@@ -2,8 +2,11 @@
     (:require [gitolite-webui.persistency :as p])
     (:use clojure.test midje.sweet))
 
-(against-background [(before :checks (p/persist-key-request "ronen" "narkisr@gmail.com" "ssh-rsa 1234"))]
-    (fact (p/ssh-pending) => (just (list {:name "ronen" :email "narkisr@gmail.com" :key "ssh-rsa 1234" })))) 
+(against-background [(before :facts (p/persist-key-request "ronen" "narkisr@gmail.com" "ssh-rsa 1234")) 
+			   (around :facts (let [typed-req (with-meta {:name "ronen" :email "narkisr@gmail.com" :key "ssh-rsa 1234" } {:type :key-request}) ] ?form))]
+    (fact (p/ssh-pending) => (just (list typed-req)))
+    (fact (p/clear-request typed-req)
+    	    (p/ssh-pending) => (just '()))) 
 
 (fact (p/access-pending) => (just (list {:name "ronen" :repo "play-0" }))
 	(against-background (before :checks (p/persist-repo-request "ronen" "play-0"))))
