6f4f12564521348e9c9397c13746212d20e13c70
narkisr <narkisr@gmail.com>
2011-09-02 23:37:07 +0300

    fixing fake file input value after error

--- src/public/fileupload.js
+++ src/public/fileupload.js
@@ -13,19 +13,25 @@ function initFileUploads() {
 	for (var i=0;i<x.length;i++) {
 		if (x[i].type != 'file') continue;
 		if (x[i].parentNode.className != 'fileinputs') continue;
+
             if(x[i].className.indexOf('error')!=-1){
-                fakeInput.className='error';
+                fakeInput.className='error';	    
             } else {
                fakeInput.className='';
             }
+           
+            fakeInput.value = x[i].getAttribute('value');
 		x[i].className = 'file hidden';
 		
-
 		var clone = fakeFileUpload.cloneNode(true);
 		x[i].parentNode.appendChild(clone);
 		x[i].relatedElement = clone.getElementsByTagName('input')[0];
 		x[i].onchange = x[i].onmouseout = function () {
+                if(this.value){
 			this.relatedElement.value = this.value;
+               } else {
+			this.relatedElement.value = this.getAttribute('value');
+               }
 		}
 	}
 }
