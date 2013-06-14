ea6ddce8f46044c70ad78b21f0d1cd2876f0d631
natto <natto@natto-tp.(none)>
2013-06-13 21:18:01 -0400

    fix bug induced to ->> form, from split to .split

--- src/gitolite_webui/gitolite.clj
+++ src/gitolite_webui/gitolite.clj
@@ -37,7 +37,7 @@
 		   (->> (slurp-conf) split-lines (remove #(.startsWith % "#")) (partition-by blank?))))
 
 (defn permissions [lines]
-  (let [pair (fn [line] (->> line (.split line #"\=") (map #(.trim %)) reverse (into [])))] 
+  (let [pair (fn [line] (->> (clojure.string/split line #"\=") (map #(.trim %)) reverse (into [])))] 
     (into {} (reduce (fn [m line] (conj m (pair line))) [] lines))))
 
 (defn parse-conf []
